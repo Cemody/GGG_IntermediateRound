4b252a8e8a7454e59bf41abc589264b227fdf7a6
Cemody <85206469+Cemody@users.noreply.github.com>
2021-06-02 11:23:12 +0800

    Add files via upload

---
+++ Inter1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progcomp;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Dexter Dela Rosa
+ */
+public class Inter1 {
+    public static void main(String[] args) {
+        String[] ar1 = {"a", "b", "c"};
+        String[] ar2 = {"1", "2", "3"};
+        System.out.println(Arrays.toString(ar1));
+        System.out.println(Arrays.toString(ar2));
+        int n = ar1.length+ar2.length;
+        int x = 0;
+        int y = 0;
+        String[] ar3 = new String[n];
+        for (int i = 0; i < n; i++) {
+            if (i%2==0) {
+                ar3[i] = ar1[x];
+                x++;
+            }else{
+                ar3[i] = ar2[y];
+                y++;
+            }
+            
+        }
+        System.out.println(Arrays.toString(ar3));
+    }
+}
